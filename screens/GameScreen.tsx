f6aebfa9378e83f28384745c0b293b7f6fd418f6
Daniel Sebastián <danielsebastiany99@gmail.com>
2024-01-05 09:28:46 -0500

    Implementacion 3

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 import insecto from '../components/insecto'
 
 const Datos=[
-    
+    { id: '1', nombre: 'Cucaracha', imagen: require('./assets/insectos/cucaracha.jpg') },
+    { id: '2', nombre: 'Mosca', imagen: require('./assets/insectos/mosca.jpg') },
 ]
 export default function GameScreen() {
   return (
