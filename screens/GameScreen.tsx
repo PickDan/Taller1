2a780b4eef6373df77c21dba36bc7dc516c05a87
Daniel Sebastián <danielsebastiany99@gmail.com>
2024-01-05 09:50:35 -0500

    Implementacion estilos

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -2,15 +2,16 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import insecto from '../components/insecto'
 
-const Datos=[
+{/*const Datos=[
     { id: '1', nombre: 'Cucaracha', imagen: require('./assets/insectos/cucaracha.jpg') },
     { id: '2', nombre: 'Mosca', imagen: require('./assets/insectos/mosca.jpg') },
-]
+]*/}
 export default function GameScreen() {
   return (
     <View>
-      <Text>Ventana Juego</Text>
-
+      <Text>CAZA DE INSECTOS</Text>
+      <Text>Puntaje: 0</Text>
+      <Text>Tiempo restante: 60 segundos</Text>
     </View>
   )
 }
--- screens/PuntajeScreen.tsx
+++ screens/PuntajeScreen.tsx
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
-export default function PuntajeScreen() {
+export default function PuntajeScreen({navigation}:any) {
   return (
     <View>
-      <Text>PuntajeScreen</Text>
+      <Text>PUNTUACIÓN ANTERIOR</Text>
+      <Text>Puntaje: {}</Text>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <Text>Volver al juego</Text>
+      </TouchableOpacity>
     </View>
   )
 }
